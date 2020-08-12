7f261dcfb2547bca6430a3ef24e6f24b2f5a7555
webpatch <webpatch@sina.com>
2020-08-12 15:02:34 +0800

    first init

---
+++ packages/cc/index.js
@@ -0,0 +1,7 @@
+const d = require("lodash/date")
+module.exports = {
+  fuck() {
+    console.log(d, `<--- d()`);
+    console.log("fuck me2333", `<--- "fuck me"`);
+  }
+}
---
+++ packages/pp/index.js
@@ -0,0 +1,2 @@
+const f = require("@zy/cc");
+f.fuck();
