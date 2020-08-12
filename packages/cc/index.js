396fd56ff09c3afda366cbbd6e3dd4f112cb1412
webpatch <webpatch@sina.com>
2020-08-12 15:18:38 +0800

    build

--- packages/cc/index.js
+++ packages/cc/index.js
@@ -2,6 +2,6 @@ const d = require("lodash/date")
 module.exports = {
   fuck() {
     console.log(d, `<--- d()`);
-    console.log("fuck me2333", `<--- "fuck me"`);
+    console.log("fuck me", `<--- "fuck me"`);
   }
 }
